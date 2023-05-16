5718df86e0ef90e2d56a7491d9ac91a5b8f2d2ed
anifredgithub <anifredthomas813@gmail.com>
2023-05-16 13:04:43 -0700

    updated

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+
+int sum(int num1, int num2);
+void prompt(char **av, char **env);
+int main(int ac, char **av, char **env);
+
+#endif /* _SHELL_H_ */
