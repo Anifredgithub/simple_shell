b4c22d634bad3a5368058e32c147abdb8f9bf889
oussamajafur <oussamajafur@gmail.com>
2023-05-24 07:05:44 -0700

    update Task3.c

--- Task3.c
+++ Task3.c
@@ -2,30 +2,32 @@
 
  int main()
  {
- char input['MAX_PATH_LENGTH'];
+ char input[MAX_PATH_LENGTH];
 
  while (1)
  {
  fgets(input, sizeof(input), stdin);
  input[strcspn(input, "\n")] = '\0';
- if (access(input, 'x_ok') == 0 || str(input,'/') != NULL)
-{
-}
+
+ if (access(input, x_ok) == 0 || stchrr(input,'/') != NULL)
+
 else
 {
-	char *path = env("PATH");
+	char *path = getenv("PATH");
 	char path_copy[MAX_PATH_LENGTH];
-	str(path_copy, path, sizeof)(path_copy);
-	char *dir = str(path_copy, ":");
-	while (dir !=NULL)
-{
-        }
-        
-        dir = str(NULL, ":");
+	strncpy(path_copy, path, sizeof(path_copy));
+	path_copy[sizeof(path_copy) - 1] = '\0';
+
+	char *dir = strtok(path_copy, ":");
+	while (dir != NULL)
+{       
+        dir = strtok(NULL, ":");
 	}
         if (dir == NULL)
 {
-	write(stder, "%s:command not availble\n",input);
-}
-}
+	write(STDERR_FILENO,, "%s:command not availble\n",input);
+   }
+  }
+ }
+  return 0;
 }
