e513aaff218a069f3c5a2d3a45a0bba8c839dead
oussamajafur <oussamajafur@gmail.com>
2023-05-24 06:25:35 -0700

    midified shell.c

--- shell.c
+++ shell.c
@@ -1,8 +1,12 @@
 #include "shell.h"
 
+int main(int argc, char**argv, char**envp)
+{
+    char input[MAX_PATH_LENGTH];
 int main(int ac, char **av, char **env)
 {
 	if (ac == 1)
 		prompt(av, env);
 	return (0);
+}
 }
