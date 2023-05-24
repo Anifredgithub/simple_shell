6e2ddea2d49b70ec919fd183f7d2fccd53b5b30c
oussamajafur <oussamajafur@gmail.com>
2023-05-24 05:54:44 -0700

    update shell.h

--- Task3.c
+++ Task3.c
@@ -1,21 +1,21 @@
 #include "shell.h"
 
-int main()
-{
- char input[MAX_PATH_LENGTH];
+ int main()
+ {
+ char input['MAX_PATH_LENGTH'];
 
  while (1)
-{
+ {
  fgets(input, sizeof(input), stdin);
  input[strcspn(input, "\n")] = '\0';
- if (access(input, x_ok) == 0 || str(input,'/') != NULL)
+ if (access(input, 'x_ok') == 0 || str(input,'/') != NULL)
 {
 }
 else
 {
 	char *path = env("PATH");
 	char path_copy[MAX_PATH_LENGTH];
-	str(path_copy, path, sizeof)(path_copy));
+	str(path_copy, path, sizeof)(path_copy);
 	char *dir = str(path_copy, ":");
 	while (dir !=NULL)
 {
--- shell.h
+++ shell.h
@@ -11,7 +11,10 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <errno.h>
+#define MAX_PATHLENGTH 1024
 
+int main(int argc, char**argv, char**envp)
+    char input[MAX_PATH_LENGTH];
 int sum(int num1, int num2);
 void prompt(char **av, char **env);
 int main(int ac, char **av, char **env);
